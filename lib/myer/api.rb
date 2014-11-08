99953640e3a94f7a82024192f4b95f6024cead75
Cornelius Schumacher <schumacher@kde.org>
2014-11-08 14:33:16 +0100

    Remove code duplication

--- lib/myer/api.rb
+++ lib/myer/api.rb
@@ -3,135 +3,79 @@ module MySelf
     attr_accessor :server
     attr_accessor :user, :password
 
-    def admin_register(pid)
+    def post(path, content = nil, auth_enabled = true)
       http = Net::HTTP.new(server, 4735)
 
-      request = Net::HTTP::Post.new("/admin/register/#{pid}")
-
-      response = http.request(request)
-
-      if response.code != "200"
-        raise "HTTP Error #{response.code} - #{response.body}"
-      else
-        json = JSON.parse(response.body)
-
-        return json["admin_id"], json["password"]
-      end
-    end
-
-    def admin_list_buckets
-      http = Net::HTTP.new(server, 4735)
-
-      request = Net::HTTP::Get.new("/admin/buckets")
-      request.basic_auth(user, password)
-
-      response = http.request(request)
-
-      if response.code != "200"
-        raise "HTTP Error #{response.code} - #{response.body}"
-      else
-        json = JSON.parse(response.body)
-
-        return json
-      end
-    end
-
-    def create_token
-      http = Net::HTTP.new(server, 4735)
-
-      path = "/tokens"
       request = Net::HTTP::Post.new(path)
-      request.basic_auth(user, password)
+      request.basic_auth(user, password) if auth_enabled
+      request.body = content if content
 
       response = http.request(request)
 
       if response.code != "200"
         raise "#{path} HTTP Error #{response.code} - #{response.body}"
       else
-        json = JSON.parse(response.body)
-
-        return json["token"]
+        return JSON.parse(response.body)
       end
     end
 
-    def register(token)
+    def get(path)
       http = Net::HTTP.new(server, 4735)
 
-      path = "/register/" + token
-      request = Net::HTTP::Post.new(path)
-
-      response = http.request(request)
-
-      if response.code != "200"
-        raise "#{path} HTTP Error #{response.code} - #{response.body}"
-      else
-        json = JSON.parse(response.body)
-
-        return json["user_id"], json["user_password"]
-      end
-    end
-
-    def create_bucket
-      http = Net::HTTP.new(server, 4735)
-
-      request = Net::HTTP::Post.new("/data")
-      request.basic_auth(user, password)
-
-      response = http.request(request)
-
-      if response.code != "200"
-        raise "HTTP Error #{response.code} - #{response.body}"
-      else
-        json = JSON.parse(response.body)
-
-        return json["bucket_id"]
-      end
-    end
-
-    def create_item(bucket_id, content)
-      http = Net::HTTP.new(server, 4735)
-
-      path = "/data/#{bucket_id}"
-      request = Net::HTTP::Post.new(path)
-      request.basic_auth(user, password)
-      request.body = content
-
-      response = http.request(request)
-
-      if response.code != "200"
-        raise "HTTP Error #{response.code} - #{response.body}"
-      else
-        json = JSON.parse(response.body)
-
-        return json["item_id"]
-      end
-    end
-
-    def get_items(bucket_id)
-      http = Net::HTTP.new(server, 4735)
-
-      path = "/data/#{bucket_id}"
       request = Net::HTTP::Get.new(path)
       request.basic_auth(user, password)
 
       response = http.request(request)
 
       if response.code != "200"
-        raise "HTTP Error #{response.code} - #{response.body}"
+        raise "#{path} HTTP Error #{response.code} - #{response.body}"
       else
-        inner_items = []
-
-        json = JSON.parse(response.body)
-
-        json.each do |json_item|
-          item = OpenStruct.new
-          item.id = json_item["item_id"]
-          item.content = json_item["content"]
-          inner_items.unshift(item)
-        end
-
-        return inner_items
+        return JSON.parse(response.body)
       end
+    end
+
+    def admin_register(pid)
+      json = post("/admin/register/#{pid}",nil,false)
+      return json["admin_id"], json["password"]
+    end
+
+    def admin_list_buckets
+      get("/admin/buckets")
+    end
+
+    def create_token
+      json = post("/tokens")
+      return json["token"]
+    end
+
+    def register(token)
+      json = post("/register/" + token, nil, false)
+      return json["user_id"], json["user_password"]
+    end
+
+    def create_bucket
+      json = post("/data")
+      return json["bucket_id"]
+    end
+
+    def create_item(bucket_id, content)
+      json = post("/data/#{bucket_id}", content)
+      return json["item_id"]
+    end
+
+    def get_items(bucket_id)
+      json = get("/data/#{bucket_id}")
+
+      inner_items = []
+
+      json.each do |json_item|
+        item = OpenStruct.new
+        item.id = json_item["item_id"]
+        item.content = json_item["content"]
+        inner_items.unshift(item)
+      end
+
+      return inner_items
     end
   end
 end
